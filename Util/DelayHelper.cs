cfbb681f77dacd959d06390e728b2c47feae6965
YoBii <xschoppmeier+github@gmail.com>
2024-08-06 16:41:18 +0200

    added DelayHelper

---
+++ Util/DelayHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LethalMoonUnlocks.Util {
+    internal class DelayHelper : MonoBehaviour {
+
+        public static DelayHelper Instance {  get; private set; }
+
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            } else {
+                Destroy(gameObject);
+            }
+        }
+        public void ExecuteAfterDelay(Action action, float delay) {
+            StartCoroutine(DelayedExecution(action, delay));
+        }
+
+        private IEnumerator DelayedExecution(Action action, float delay) {
+            yield return new WaitForSeconds(delay);
+            action.Invoke();
+        }
+    }
+}
