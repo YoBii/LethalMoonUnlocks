83f49812d1fe477847df8752d463ee9d628902fe
YoBii <xschoppmeier+github@gmail.com>
2024-08-06 16:41:02 +0200

    added LethalQuantities compatibility

---
+++ Compatibility/LQCompatibility.cs
@@ -0,0 +1,23 @@
+﻿using LethalQuantities.Json;
+using LethalQuantities.Objects;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace LethalMoonUnlocks.Compatibility {
+    internal static class LQCompatibility {
+        public static string GetLQRiskLevel(LMUnlockable unlock) {
+            Type type = LethalQuantities.Plugin.INSTANCE.GetType();
+            FieldInfo field = type.GetField("presets", BindingFlags.NonPublic | BindingFlags.Instance);
+            Dictionary<Guid, LevelPreset> presets = (Dictionary<Guid, LevelPreset>)field.GetValue(LethalQuantities.Plugin.INSTANCE);
+            LevelPreset preset = presets.TryGetValue(unlock.ExtendedLevel.SelectableLevel.getGuid(), out LevelPreset value) ? value : null;
+
+            if (preset != null) {
+                return preset.riskLevel.value;
+            } else {
+                return string.Empty;
+            }
+        }
+    }
+}
