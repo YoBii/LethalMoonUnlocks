e167a3227337475fbe8c051bef9b23072146ecbd
YoBii <xschoppmeier+github@gmail.com>
2024-08-17 12:40:54 +0200

    subscribe to LethalConstellations route event; try fix TypeLoadExceptions

--- Compatibility/LethalConstellationsExtension.cs
+++ Compatibility/LethalConstellationsExtension.cs
@@ -1,29 +1,75 @@
 ﻿using LethalConstellations.PluginCore;
+using LethalQuantities;
 using LethalMoonUnlocks.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using LethalQuantities.Json;
+using UnityEngine.Rendering.HighDefinition;
+using HarmonyLib;
+using System.Runtime.CompilerServices;
 
 namespace LethalMoonUnlocks.Compatibility {
     internal class LethalConstellationsExtension {
-        internal static void ApplyUnlocks() {
-            if (Plugin.LethalConstellationsPresent) {
-                if (ConfigManager.DiscoveryMode) {
-                    ApplyVisibility();
-                    ApplyDefaultMoons();
-                    AddDiscoveryCount();
-                    HideUnlocksNotInCurrentConstellation();
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        internal LethalConstellationsExtension() {
+            LethalConstellations.EventStuff.NewEvents.RouteConstellationSuccess.AddListener(BuyConstellations);
+            //Constellations = Collections.ConstellationStuff;
+        }
+        
+        //[NonSerialized]
+        //List<ClassMapper> Constellations;
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        internal void ApplyUnlocks() {
+            if (ConfigManager.DiscoveryMode) {
+                ApplyVisibility();
+                ApplyDefaultMoons();
+                AddDiscoveryCount();
+                HideUnlocksNotInCurrentConstellation();
+            } else {
+                HideUnlocksNotInCurrentConstellation();
+                ShowUnlocksInCurrentConstellation();
+            }
+            if (ConfigManager.LethalConstellationsOverridePrice) {
+                ApplyPrices();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        internal string GetConstellationName(LMUnlockable unlock) {
+            ClassMapper constellation = Collections.ConstellationStuff.Where(con => con.constelMoons.Any(moon => moon == unlock.Name)).FirstOrDefault();
+            if (constellation != null) {
+                return constellation.consName; 
+            }
+            return string.Empty;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        internal void BuyConstellations() {
+            var currentConstellation = Collections.ConstellationStuff.Where(c => c.consName == Collections.CurrentConstellation).FirstOrDefault();
+            var unlock = UnlockManager.Instance.Unlocks.Where(unlock => unlock.ExtendedLevel.NumberlessPlanetName == currentConstellation.defaultMoon).FirstOrDefault();
+            if (unlock != null) {
+                Plugin.Instance.Mls.LogInfo($"Routing to constellation {currentConstellation.consName} -> default moon {unlock.Name} with ID {unlock.ExtendedLevel.SelectableLevel.levelID}!");
+                if (unlock.ExtendedLevel.RoutePrice > 0) {
+                    if (ConfigManager.LethalConstellationsOverridePrice) {
+                        Plugin.Instance.Mls.LogInfo($"Route to {unlock.Name} was paid ({unlock.ExtendedLevel.RoutePrice} credits).");
+                    }
+                    if (NetworkManager.Instance.IsServer()) {
+                        UnlockManager.Instance.BuyMoon(unlock.Name);
+                    } else {
+                        NetworkManager.Instance.ClientBuyMoon(unlock.Name);
+                    }
                 } else {
-                    HideUnlocksNotInCurrentConstellation();
-                    ShowUnlocksInCurrentConstellation();
-                }
-                if (ConfigManager.LethalConstellationsOverridePrice) {
-                    ApplyPrices();
+                    Plugin.Instance.Mls.LogInfo($"Route to {unlock.Name} was free.");
                 }
             }
         }
-        private static void ApplyVisibility() {
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public void ApplyVisibility() {
             foreach (ClassMapper constellation in Collections.ConstellationStuff) {
                 if (constellation.constelMoons.Any(constellation => UnlockManager.Instance.Unlocks.Where(unlock => unlock.Discovered).Any(unlock => constellation == unlock.Name))) {
                     if (constellation.isHidden == true && NetworkManager.Instance.IsServer()) {
@@ -39,14 +85,18 @@ namespace LethalMoonUnlocks.Compatibility {
                 }
             }
         }
-        private static void ApplyDefaultMoons() {
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public void ApplyDefaultMoons() {
             foreach (ClassMapper constellation in Collections.ConstellationStuff.Where(constellation => constellation.isHidden == false)) {
                 var constellationUnlocks = UnlockManager.Instance.Unlocks.Where(unlock => unlock.Discovered && constellation.constelMoons.Any(moon => unlock.Name == moon)).OrderBy(unlock => unlock.ExtendedLevel.RoutePrice).ToList();
                 constellation.defaultMoon = constellationUnlocks.First().Name;
                 Plugin.Instance.Mls.LogDebug($"Constellation {constellation.consName}: set default moon to {constellationUnlocks.First().Name} ({constellation.defaultMoon})");
             }
         }
-        private static void AddDiscoveryCount() {
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public void AddDiscoveryCount() {
             foreach (ClassMapper constellation in Collections.ConstellationStuff.Where(constellation => constellation.isHidden == false)) {
                 var constellationUnlocks = UnlockManager.Instance.Unlocks.Where(unlock => unlock.Discovered && constellation.constelMoons.Any(moon => unlock.Name == moon)).ToList();
                 constellation.optionalParams = $"\nMoons discovered: {constellationUnlocks.Count}";
@@ -55,7 +105,9 @@ namespace LethalMoonUnlocks.Compatibility {
                 }
             }
         }
-        private static void ApplyPrices() {
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void ApplyPrices() {
             foreach (ClassMapper constellation in Collections.ConstellationStuff.Where(constellation => constellation.isHidden == false)) {
                 var constellationDefaultMoonUnlock = UnlockManager.Instance.Unlocks.Where(unlock => unlock.Name == constellation.defaultMoon).FirstOrDefault();
                 if (constellationDefaultMoonUnlock != null) {
@@ -64,7 +116,9 @@ namespace LethalMoonUnlocks.Compatibility {
                 }
             }
         }
-        private static void HideUnlocksNotInCurrentConstellation() {
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void HideUnlocksNotInCurrentConstellation() {
             var currentConstellation = Collections.ConstellationStuff.Where(constellation => constellation.consName == Collections.CurrentConstellation).FirstOrDefault();
             if (currentConstellation == null) return;
             foreach (var unlock in UnlockManager.Instance.Unlocks) {
@@ -74,7 +128,9 @@ namespace LethalMoonUnlocks.Compatibility {
                 }
             }
         }
-        private static void ShowUnlocksInCurrentConstellation() {
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void ShowUnlocksInCurrentConstellation() {
             var currentConstellation = Collections.ConstellationStuff.Where(constellation => constellation.consName == Collections.CurrentConstellation).FirstOrDefault();
             if (currentConstellation == null) return;
             foreach (var unlock in UnlockManager.Instance.Unlocks) {
