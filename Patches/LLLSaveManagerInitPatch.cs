c32542421459e2791118a4009120beeef74cde6a
YoBii <xschoppmeier+github@gmail.com>
2025-02-13 23:03:11 +0100

    add LLL patch to get defaults before LLL loads its save

---
+++ Patches/LLLSaveManagerInitPatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using HarmonyLib;
+
+namespace LethalMoonUnlocks.Patches {
+    [HarmonyPatch]
+    internal class LLLSaveManagerInitPatch {
+        private static MethodBase TargetMethod() {
+            var type = AccessTools.TypeByName("LethalLevelLoader.SaveManager");
+            return AccessTools.Method(type, "InitializeSave");
+        }
+
+        private static void Prefix() {
+            UnlockManager.Instance.InitializeUnlocks();
+        }
+    }
+}
