d394a43119aab1cf5e2a4980e9fb896539d94a45
YoBii <22051355+YoBii@users.noreply.github.com>
2026-03-21 21:38:19 +0100

    style: improve JLL/Wesley moon unlock alert wording to fit better with LMU

---
+++ Compatibility/JLLCompatibility.cs
@@ -0,0 +1,44 @@
+﻿using LethalMoonUnlocks.Util;
+
+namespace LethalMoonUnlocks.Compatibility {
+    internal class JLLCompatibility {
+        internal static void ReplaceJLLAlertDiscovery(string text) {
+            if (NetworkManager.Instance == null || !NetworkManager.Instance.IsServer()) {
+                return;
+            }
+
+            string locationName = text.Replace("Location: ", string.Empty);
+            NetworkManager.Instance.ServerSendAlertMessage(new Notification() {
+                Header = locationName,
+                Text = "New location! Adding to moons catalog...",
+                IsWarning = false,
+                UseSave = false,
+                Key = "LMU_JLL_Discovery_1"
+            });
+            NetworkManager.Instance.ServerSendAlertMessage(new Notification() {
+                Header = locationName,
+                Text = "Error: Route unavailable!\nAdding to backlog...",
+                IsWarning = true,
+                UseSave = false,
+                Key = "LMU_JLL_Discovery_2"
+            });
+            NetworkManager.Instance.ServerSendAlertQueueEvent();
+        }
+        
+        internal static void ReplaceJLLAlert(string text) {
+            if (NetworkManager.Instance == null || !NetworkManager.Instance.IsServer()) {
+                return;
+            }
+
+            string locationName = text.Replace("Location: ", string.Empty);
+            NetworkManager.Instance.ServerSendAlertMessage(new Notification() {
+                Header = locationName,
+                Text = "New location! Adding to moons catalog...",
+                IsWarning = false,
+                UseSave = false,
+                Key = "LMU_JLL_1"
+            });
+            NetworkManager.Instance.ServerSendAlertQueueEvent();
+        }
+    }
+}
--- Patches/HUDManagerPatch.cs
+++ Patches/HUDManagerPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using LethalMoonUnlocks.Compatibility;
 using LethalMoonUnlocks.Util;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,15 @@ namespace LethalMoonUnlocks.Patches {
             if (!ConfigManager.AlertMessageQueueing)
                 return true;
             if (!HUDManager.Instance.CanTipDisplay(isWarning, useSave, prefsKey)) {
+                return false;
+            }
+            if (!string.IsNullOrEmpty(bodyText) && headerText == "Route Discovered!" && bodyText.StartsWith("Location: ")) {
+                if (ConfigManager.DiscoveryMode) {
+                    JLLCompatibility.ReplaceJLLAlertDiscovery(bodyText);
+                }
+                else {
+                    JLLCompatibility.ReplaceJLLAlert(bodyText);
+                }
                 return false;
             }
             if (!prefsKey.StartsWith("LMU_")) {
