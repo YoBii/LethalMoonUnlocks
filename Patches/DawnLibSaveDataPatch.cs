041d7ce8cb1576cc4c4371736795600c5d09d43d
YoBii <22051355+YoBii@users.noreply.github.com>
2026-03-30 22:43:51 +0200

    fix: restore original values before dawn saves

---
+++ Patches/DawnLibSaveDataPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System.Linq;
+using System.Reflection;
+
+namespace LethalMoonUnlocks.Patches {
+    [HarmonyPatch]
+    internal class DawnLibSaveDataPatch {
+        private static MethodBase TargetMethod() {
+            if (!Plugin.DawnLibPresent) {
+                return null;
+            }
+
+            var type = AccessTools.TypeByName("Dawn.Internal.DawnNetworker");
+            return AccessTools.Method(type, "SaveData");
+        }
+
+        private static void Prefix() {
+            if (NetworkManager.Instance == null || !NetworkManager.Instance.IsServer() || UnlockManager.Instance == null) {
+                return;
+            }
+
+            UnlockManager.Instance.Unlocks.Do(unlock => unlock.RestoreOriginalState());
+        }
+
+        private static void Postfix() {
+            if (NetworkManager.Instance == null || !NetworkManager.Instance.IsServer() || UnlockManager.Instance == null) {
+                return;
+            }
+
+            foreach (var unlock in UnlockManager.Instance.Unlocks) {
+                unlock.ApplyState();
+                unlock.ApplyVisibility();
+            }
+        }
+    }
+}
